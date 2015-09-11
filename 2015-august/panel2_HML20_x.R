1a0cbdcd1fe317d20faa13e450d98b88ac5406ce
John Alberg <alberg@euclidean.com>
2015-09-11 16:18:52 -0500

    Adding files.

---
+++ 2015-august/panel2_HML20_x.R
@@ -0,0 +1,181 @@
+# library for 
+library(stringr)
+library(scales)
+library(ggthemes)
+library(PerformanceAnalytics)
+library(RColorBrewer)
+
+# Globals to set for data end date (check the data file for last date)
+end.year  <- 2015
+end.month <- 6
+
+# The URL for the data.
+# http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/25_Portfolios_5x5_CSV.zip
+url.name  <- paste("http://mba.tuck.dartmouth.edu",
+                   "pages/faculty/ken.french/ftp", sep="/")
+file.name <- "25_Portfolios_5x5_CSV.zip"
+
+# Download the data and unzip it
+ff.url <- paste(url.name, file.name, sep="/")
+f <- tempfile()
+download.file(ff.url, f)
+file.list <- unzip(f, list=TRUE)
+ 
+# Parse the data
+fama.data <- read.csv(unzip(f,files=as.character(file.list[1,1])),
+                      skip=19,
+                      header=TRUE,
+                      stringsAsFactors=FALSE)
+names(fama.data)[[1]] <- "DATE"
+
+# Now we want to remove all the data below the end date
+
+start.year  <- as.numeric(substr(fama.data$DATE[[1]],1,4))
+start.month <- as.numeric(substr(fama.data$DATE[[1]],5,6))
+num.rows <- 12*(end.year-start.year)+(end.month-start.month)+1
+fama.data <- head(fama.data,num.rows)
+
+date.seq <- as.Date(paste(fama.data$DATE,"01",sep=""),"%Y%m%d")
+fama.data$DATE <- date.seq
+
+# Transform the data soe that the return cells are numeric decimal format
+
+for (i in 2:ncol(fama.data)) fama.data[,i] <- as.numeric(str_trim(fama.data[,i]))
+for (i in 2:ncol(fama.data)) fama.data[,i] <- fama.data[,i]/100
+
+# Now we calculate the the HML sequence from the factor sequences
+# Column names are: DATE SMALL.LoBM ME1.BM2 SMALL.HiBM BIG.LoBM ME2.BM2 BIG.HiBM
+fd <- fama.data
+
+fama.data$Hi <- 0.2*( fd$SMALL.HiBM + fd$ME2.BM5 + fd$ME3.BM5 + fd$ME4.BM5 + fd$BIG.HiBM )
+fama.data$Lo <- 0.2*( fd$SMALL.LoBM + fd$ME2.BM1 + fd$ME3.BM1 + fd$ME4.BM1 + fd$BIG.LoBM )
+
+width <- 5*12
+
+# Now create a time series of the HML data that we can pass off to apply.rolling and other PerformanceAnalytics functions
+
+# So that we can use geometric returns
+Return.custom <- function(R, scale = NA, geometric = TRUE) {
+    Return.annualized(R, scale = NA, geometric = TRUE)
+}
+
+ts.data <- data.frame(fama.data$Hi)
+row.names(ts.data) <- date.seq
+
+z <- apply.rolling(ts.data, width=width, FUN = "Return.custom")
+fama.data$Hi.5YrRolling <- z[[1]]
+
+ts.data <- data.frame(fama.data$Lo)
+row.names(ts.data) <- date.seq
+
+z <- apply.rolling(ts.data, width=width, FUN = "Return.custom")
+fama.data$Lo.5YrRolling <- z[[1]]
+
+fama.data$Hi.Lo.5YrRolling <- fama.data$Hi.5YrRolling - fama.data$Lo.5YrRolling 
+
+rows <- nrow(fama.data)
+
+start.indexes <- c()
+end.indexes   <- c()
+
+i <- 60
+
+while ( i < rows ) {
+
+    if (fama.data$Hi.Lo.5YrRolling[i]<0) {
+
+        j         <- i+1
+        start.idx <- i
+        end.idx   <- j
+        min.val   <- fama.data$Hi.Lo.5YrRolling[i]
+        max.val   <- fama.data$Hi.Lo.5YrRolling[i]
+
+        while ( j < i + 12*8) {
+
+            if ( j < rows ) {
+                if (fama.data$Hi.Lo.5YrRolling[j]>max.val) {
+                    max.val <- fama.data$Hi.Lo.5YrRolling[j]
+                    end.idx <- j
+                }
+
+            } else {
+                    max.val <- fama.data$Hi.Lo.5YrRolling[rows]
+                    end.idx <- rows
+            }
+
+            j <- j+1
+
+        }
+
+        start.indexes <- c(start.indexes,start.idx)
+        end.indexes   <- c(end.indexes,end.idx)
+        
+        str <- paste("Start=",fama.data$DATE[start.idx],
+                     "End=",fama.data$DATE[end.idx],
+                     "EndValue=",fama.data$Hi.Lo.5YrRolling[end.idx],
+                     "Duration=",(end.idx-start.idx)/12)
+        print(str)
+
+        # i <- j - 12*3
+        i <- end.idx + 1
+        
+    } else {
+
+        i <- i+1
+
+    }
+
+}
+                                 # Create Series
+series <- c()
+months <- c()
+values <- c()
+
+for (i in 3:length(start.indexes)) {
+
+    start.idx <- start.indexes[i]
+    end.idx   <- end.indexes[i]
+    str       <- paste(format(fama.data$DATE[start.idx], "%Y"),
+                       format(fama.data$DATE[end.idx]  , "%Y"), sep="-")
+    hi.val <- 0
+    lo.val <- 0
+    
+    series <- c(series,str)
+    months <- c(months,1)
+    values <- c(values,0)
+    
+    for (j in start.idx:end.idx) {
+
+        hi.val <- (hi.val+1)*(fama.data$Hi[j]+1)-1
+        lo.val <- (lo.val+1)*(fama.data$Lo[j]+1)-1
+        cur.value <- hi.val - lo.val
+        # cur.value <- cur.value + fama.data$Hi.Lo[j]
+        
+        series <- c(series,str)
+        months <- c(months,j-start.idx+2)
+        values <- c(values,cur.value)
+
+    }
+    
+}
+
+recov.data <- data.frame(series,months,values)
+
+colors <- rev( brewer.pal(7,"Blues") )[1:5]
+colors <- c(colors, '#DD592D' )
+
+g <- ggplot(recov.data,aes(x=months,y=values,size=series,group=series,colour=series))
+g <- g + geom_hline(aes(yintercept=0.0),color="grey",linetype=1) 
+g <- g + geom_line()
+g <- g + scale_size_manual(values=c(1,1,1,1,1,2))
+g <- g + scale_colour_manual(values=colors )
+
+#g <- g + scale_x_discrete( "", breaks=c(1,12,24,48,72,96), labels=c("1mo","1yr","2yrs","4yrs","6yrs","8yrs") )
+g <- g + scale_x_discrete( "", breaks=c( 1, 25, 50, 75 ) )
+g <- g + scale_y_continuous( "", labels=percent )
+g <- g + guides(fill=FALSE)
+
+g + theme_bw() + theme(legend.position="none",
+                       panel.background=element_blank() ,
+                       panel.border=element_blank() )
+
---
+++ 2015-march/perf_and_drawdowns.r
@@ -0,0 +1,61 @@
+#!/usr/bin/env Rscript
+# Prototype R script for use at command line in Linux, Mac OS X, UNIX
+
+library(zoo);
+library(xts);
+library(PerformanceAnalytics);
+library(ggplot2);
+
+# Load Data
+data <- read.table("sim_results-0.dat", sep = " ", header = TRUE);
+
+ey <- data$EY;
+sp500 <- data$SP500;
+
+dates = seq(from=as.Date("1973-01-01"), by = "month", length.out=NROW(ey));
+
+#data_ts <- ts( cbind(ey,sp500), start = c(1973,1), frequency=12);
+
+data_ts <- cbind(ey,sp500);
+data_xts <- xts( data_ts, order.by=dates );
+
+ey_ts <- ts(ey,start = c(1973,1), frequency=12);
+ey_xts <- as.xts( ey_ts );
+
+sp500_ts <- ts(sp500,start = c(1973,1), frequency=12);
+sp500_xts <- as.xts( sp500_ts );
+
+ex <- ey - sp500;
+
+# dates  = seq(from=as.Date("1973-01-01"), by = "month", length.out=NROW(ex));
+ex_xts = xts(ex,order.by=dates);
+
+pdf("performance.pdf");
+
+# proxima nova font
+
+par(mfrow=c(2,1),mar=c(1,4,2,1), oma=c(2,1,2,1));
+
+chart.CumReturns(data_xts,
+                 main="Value Investing: Simulated Performance and Active Drawdowns",
+                 cex.main=1.1,
+                 sub="",
+                 ylab="Growth of $1 Invested",
+                 geometric=TRUE,
+                 wealth.index=TRUE,
+                 xlab="",
+		 col=c('#34BBA7','#617994') );
+
+legend(100,500,
+       legend = c("Value Approach", "SP500 Total Return"),
+       col=c('#34BBA7','#617994'),
+       lty = c("solid", "solid"),
+       lwd = c( 2, 2),
+       xjust = .5, yjust = .5)
+
+chart.Drawdown(ex_xts,ylab="Drawdown Relative to SP500 TR",col=c('#DD592D'),yaxis=FALSE);
+
+yLabels <- c(-.5, -.4, -.3, -.2, -.1, 0)
+axis(2, at=yLabels, labels=sprintf(round(100*yLabels), fmt="%d%%") )
+
+dev.off();
